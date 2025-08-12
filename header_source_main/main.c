46371eede5c5c612fd363c7d303a5be43bf822c0
mechacoredev <eneserbulut67890@gmail.com>
2025-08-12 17:53:28 +0300

    Update main.c

--- header_source_main/main.c
+++ header_source_main/main.c
@@ -41,19 +41,23 @@
 
 /* Private variables ---------------------------------------------------------*/
 I2C_HandleTypeDef hi2c1;
+DMA_HandleTypeDef hdma_i2c1_rx;
+
+TIM_HandleTypeDef htim6;
 
 /* USER CODE BEGIN PV */
 ADXL345_t my_adxl345;
 ADXL345_CONFIG_t my_adxl345_config;
 uint8_t adxl345add;
 ADXL345_RETURN_STAT adxlstat1,adxlstat2;
-uint16_t lasttime=0;
 /* USER CODE END PV */
 
 /* Private function prototypes -----------------------------------------------*/
 void SystemClock_Config(void);
 static void MX_GPIO_Init(void);
+static void MX_DMA_Init(void);
 static void MX_I2C1_Init(void);
+static void MX_TIM6_Init(void);
 /* USER CODE BEGIN PFP */
 
 /* USER CODE END PFP */
@@ -92,10 +96,12 @@ int main(void)
 
   /* Initialize all configured peripherals */
   MX_GPIO_Init();
+  MX_DMA_Init();
   MX_I2C1_Init();
+  MX_TIM6_Init();
   /* USER CODE BEGIN 2 */
   my_adxl345.i2c_handler=&hi2c1;
-  my_adxl345_config.ADXL345_BWRATE.RATE=BANDWIDTHRATE_800_0;
+  my_adxl345_config.ADXL345_BWRATE.RATE=BANDWIDTHRATE_25_0;
   my_adxl345_config.ADXL345_BWRATE.LOW_PWR=0;
   my_adxl345_config.ADXL345_POWERCNTRL.LINK=0;
   my_adxl345_config.ADXL345_POWERCNTRL.AUTO_SLEEP=0;
@@ -108,9 +114,18 @@ int main(void)
   my_adxl345_config.ADXL345_DATAFRMT.FULL_RES=0;
   my_adxl345_config.ADXL345_DATAFRMT.JUSTIFY=0;
   my_adxl345_config.ADXL345_DATAFRMT.RANGE=RANGE_4G;
+  my_adxl345_config.ADXL345_INT_ENABLE.ACTIVITY=0;
+  my_adxl345_config.ADXL345_INT_ENABLE.DATA_READY=0;
+  my_adxl345_config.ADXL345_INT_ENABLE.DOUBLE_TAP=0;
+  my_adxl345_config.ADXL345_INT_ENABLE.FREE_FALL=0;
+  my_adxl345_config.ADXL345_INT_ENABLE.INACTIVITY=0;
+  my_adxl345_config.ADXL345_INT_ENABLE.OVERRUN=0;
+  my_adxl345_config.ADXL345_INT_ENABLE.SINGLE_TAP=0;
+  my_adxl345_config.ADXL345_INT_ENABLE.WATERMARK=0;
   adxl345add=ADXL345_AutoDetect(&my_adxl345);
   adxlstat1=ADXL345_Init(&my_adxl345, adxl345add);
   adxlstat2=ADXL345_Configuration(&my_adxl345, &my_adxl345_config);
+  HAL_TIM_Base_Start_IT(&htim6);
   /* USER CODE END 2 */
 
   /* Infinite loop */
@@ -120,9 +135,7 @@ int main(void)
     /* USER CODE END WHILE */
 
     /* USER CODE BEGIN 3 */
-	  ADXL345_Read_Polling(&my_adxl345);
-	  ADXL345_GetValue(&my_adxl345);
-	  HAL_Delay(20);
+
   }
   /* USER CODE END 3 */
 }
@@ -189,7 +202,7 @@ static void MX_I2C1_Init(void)
 
   /* USER CODE END I2C1_Init 1 */
   hi2c1.Instance = I2C1;
-  hi2c1.Init.ClockSpeed = 400000;
+  hi2c1.Init.ClockSpeed = 100000;
   hi2c1.Init.DutyCycle = I2C_DUTYCYCLE_2;
   hi2c1.Init.OwnAddress1 = 0;
   hi2c1.Init.AddressingMode = I2C_ADDRESSINGMODE_7BIT;
@@ -204,6 +217,60 @@ static void MX_I2C1_Init(void)
   /* USER CODE BEGIN I2C1_Init 2 */
 
   /* USER CODE END I2C1_Init 2 */
+
+}
+
+/**
+  * @brief TIM6 Initialization Function
+  * @param None
+  * @retval None
+  */
+static void MX_TIM6_Init(void)
+{
+
+  /* USER CODE BEGIN TIM6_Init 0 */
+
+  /* USER CODE END TIM6_Init 0 */
+
+  TIM_MasterConfigTypeDef sMasterConfig = {0};
+
+  /* USER CODE BEGIN TIM6_Init 1 */
+
+  /* USER CODE END TIM6_Init 1 */
+  htim6.Instance = TIM6;
+  htim6.Init.Prescaler = 8399;
+  htim6.Init.CounterMode = TIM_COUNTERMODE_UP;
+  htim6.Init.Period = 2499;
+  htim6.Init.AutoReloadPreload = TIM_AUTORELOAD_PRELOAD_DISABLE;
+  if (HAL_TIM_Base_Init(&htim6) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  sMasterConfig.MasterOutputTrigger = TIM_TRGO_RESET;
+  sMasterConfig.MasterSlaveMode = TIM_MASTERSLAVEMODE_DISABLE;
+  if (HAL_TIMEx_MasterConfigSynchronization(&htim6, &sMasterConfig) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  /* USER CODE BEGIN TIM6_Init 2 */
+
+  /* USER CODE END TIM6_Init 2 */
+
+}
+
+/**
+  * Enable DMA controller clock
+  */
+static void MX_DMA_Init(void)
+{
+
+  /* DMA controller clock enable */
+  __HAL_RCC_DMA1_CLK_ENABLE();
+
+  /* DMA interrupt init */
+  /* DMA1_Stream0_IRQn interrupt configuration */
+  HAL_NVIC_SetPriority(DMA1_Stream0_IRQn, 0, 0);
+  HAL_NVIC_EnableIRQ(DMA1_Stream0_IRQn);
 
 }
 
@@ -229,6 +296,17 @@ static void MX_GPIO_Init(void)
 
 /* USER CODE BEGIN 4 */
 
+void HAL_TIM_PeriodElapsedCallback(TIM_HandleTypeDef *htim){
+	if(htim->Instance==TIM6){
+		ADXL345_Read_DMA(&my_adxl345);
+	}
+}
+
+void HAL_I2C_MemRxCpltCallback(I2C_HandleTypeDef *hi2c){
+	if(hi2c->Instance==I2C1){
+		ADXL345_GetValue(&my_adxl345);
+	}
+}
 /* USER CODE END 4 */
 
 /**
